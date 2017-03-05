44843fe999f0f8046f94259bcb166abd97c27508
Adel <lqj679ssn@qq.com>
2017-03-05 23:46:05 +0800

    wsgi

--- QingningWork/wsgi.py
+++ QingningWork/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "QingningWork.settings")
+
+sys.path.append('/home/ubuntu/adel/git/QingningWork')
 
 application = get_wsgi_application()
