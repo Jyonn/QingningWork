50d4e50a32c40e9dabf6f4ded7f985125fb8056f
adelliu <lqj679ssn@qq.com>
2017-03-21 11:19:41 +0800

    验证码

--- BaseFunc/img_captcha.py
+++ BaseFunc/img_captcha.py
@@ -10,16 +10,15 @@ from PIL import Image, ImageDraw, ImageFont, ImageFilter
 _letter_cases = "abcdefghjkmnpqrstuvwxy"  # 小写字母，去除可能干扰的i，l，o，z
 _upper_cases = _letter_cases.upper()  # 大写字母
 _numbers = ''.join(map(str, range(3, 10)))  # 数字
-# init_chars = ''.join((_letter_cases, _upper_cases, _numbers))
-init_chars = _numbers
-fontType = os.path.join(os.path.dirname(__file__), 'Keyboard.ttf')  # 要注意修改路径
+init_chars = ''.join((_letter_cases, _upper_cases, _numbers))
+# init_chars = _numbers
+fontType = os.path.join(os.path.dirname(__file__), 'AGALEGA-Regular.otf')  # 要注意修改路径
 
 
 def create_validate_code(size=(120, 30),
                          chars=init_chars,
-                         img_type="GIF",
                          mode="RGBA",
-                         bg_color=(255, 255, 255, 1),
+                         bg_color=(255, 255, 255, 0),
                          fg_color=(0, 0, 255),
                          font_size=18,
                          font_type=fontType,
@@ -97,7 +96,7 @@ def create_points(draw, point_chance, width, height):
 def create_strs(draw, chars, length, font_type, font_size, width, height, fg_color):
     """绘制验证码字符"""
     c_chars = random.sample(chars, length)
-    strs = ' %s ' % ' '.join(c_chars)  # 每个字符前后以空格隔开
+    strs = ' %s ' % ''.join(c_chars)  # 每个字符前后以空格隔开
 
     font = ImageFont.truetype(font_type, font_size)
     font_width, font_height = font.getsize(strs)
