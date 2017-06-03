648d6216a488eb29bdf1e1e555fdb848431139b0
Adel <lqj679ssn@qq.com>
2017-06-03 14:51:50 +0800

    height

--- static_files/v2/js/work.js
+++ static_files/v2/js/work.js
@@ -6,16 +6,12 @@ function resize() {
     $('.preview-content').each(function () {
         this.style.height = "auto";
         this.style.height = this.scrollHeight + 'px';
-        alert(this.style.height);
-        alert(this.scrollHeight);
         if (this.style.height !== this.scrollHeight + 'px') {
             this.style.height = this.scrollHeight + 'px';
-            alert(this.style.height);
-            alert(this.scrollHeight);
         }
     }).on('input', function () {
         this.style.height = "auto";
-        this.style.height = (this.scrollHeight) + 'px';
+        this.style.height = this.scrollHeight + 'px';
     });
 }
 
